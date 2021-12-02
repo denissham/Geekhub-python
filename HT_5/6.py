dc095e434c3a2c61d1c690a4e6521e42adece6ce
DESKTOP-B5L1KHO\Denis <shamd1989@gmail.com>
2021-12-02 17:49:07 +0200

    Adding more requirements of range function

--- HT_5/6.py
+++ HT_5/6.py
@@ -9,59 +9,58 @@ def func(*args):
 		start = 0
 		stop = int(args[0])
 		step = 1
-		result = start
+			
+		if stop <= 0:
+			return False
 
-		if step > 0:
+		else:
 			while result < stop:
 				yield result
 				result += step
-
-		elif step < 0 and stop < 0:
-			while result > stop:
-				yield result
-				result += step
-
-		elif step <= 0 and stop >= 0:
-			return False
+			
 		
 	elif len(args) == 2:
 		start = int(args[0])
 		stop = int(args[1])
 		step = 1
-		result = start
-		if step > 0:
+
+		if stop <=0 and start <=0 and start>stop:
+			return False
+			
+		else:
+			result = start
 			while result < stop:
 				yield result
-				result += step
-
-		elif step < 0 and stop < 0:
-			while result > stop:
-				yield result
-				result += step
-
-		elif step <= 0 and stop >= 0:
-			return False
-		
+				result += step	
 		
 	elif len(args) >= 3:
 		start = int(args[0])
 		stop = int(args[1])
 		step = int(args[2])
-		result = start
 		if step > 0:
+			result = start
 			while result < stop:
 				yield result
 				result += step
 
 		elif step < 0 and stop < 0:
+			result = start
 			while result > stop:
 				yield result
 				result += step
 
-		elif step <= 0 and stop >= 0:
+		elif step < 0 and stop > 0 and stop < start:
+			result = start
+			while result > stop:
+				yield result
+				result += step
+
+		elif step <= 0 and stop >= 0 and stop > start:
 			return False
 
-f = func(10)
+
+
+f = func(30,20,-1)
 
 print(next(f))
 print(next(f))
@@ -76,5 +75,4 @@ print(next(f))
 print(next(f))
 print(next(f))
 print(next(f))
-
 
