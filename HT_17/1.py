321639e8807aeac8da5cb8369c44157ad2deb091
DESKTOP-B5L1KHO\Denis <shamd1989@gmail.com>
2022-01-23 19:29:34 +0200

    Adding EC to wait till input field is active and clickable

--- HT_17/1.py
+++ HT_17/1.py
@@ -11,14 +11,16 @@ import os
 
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class SubmitForm(object):
 		
 	def form_manipulation(self):
 		wd = Chrome()
+		wait = WebDriverWait(wd, 30)
 		form_page = wd.get("https://docs.google.com/forms/d/e/1FAIpQLScLhHgD5pMnwxl8JyRfXXsJekF8_pDG36XtSEwaGsFdU2egyw/viewform?usp=sf_link")
-		input_field = wd.find_element(By.CSS_SELECTOR, 'input[jsname="YPqjbf"]')
-		print(input_field.text)
+		input_field = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'input[jsname="YPqjbf"]')))
 		input_field.send_keys("Denys Shamov")
 		wd.save_screenshot("filled_form.png")
 		submit_btn = wd.find_element(By.CSS_SELECTOR, 'div[jsname="M2UYVd"]')
