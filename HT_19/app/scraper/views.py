ddf2e20a8d6dc9922cf57029e4b4d270b31dde8f
DESKTOP-B5L1KHO\Denis <shamd1989@gmail.com>
2022-02-02 19:09:25 +0200

    Added fix to scrape  only stories not existing in db

--- HT_19/app/scraper/views.py
+++ HT_19/app/scraper/views.py
@@ -10,17 +10,47 @@ def index(request):
 	category_name = request.POST.get('category')
 	print(category_name)
 	stories = get_stories(category_name)
-	print(stories)
-	stories_list = get_stories_list(stories)
-	values_to_db = write_db(category_name, stories_list)
-	print("All is done! Thank you!")
-	return render(request, 'scraper/index.html')
+	if len(stories)==0:
+		print("There is nothing to add to DB")
+		return render(request, 'scraper/index.html')
+	else:
+		print(stories)
+		stories_list = get_stories_list(stories)
+		values_to_db = write_db(category_name, stories_list)
+		print("All is done! Thank you!")
+		return render(request, 'scraper/index.html')
 
 def get_stories(category_name):
 	url = f"https://hacker-news.firebaseio.com/v0/{category_name}.json?print=pretty"
 	r = requests.get(url)
 	response_value = r.json()
-	return response_value
+	verified_response = []
+	if category_name == "askstories":
+		for s_id in response_value:
+			try:
+				story_in_db_check = Ask.objects.get(story_id = s_id)
+			except:
+				verified_response.append(s_id)
+	elif category_name =="showstories":
+		for s_id in response_value:
+			try:
+				story_in_db_check = Show.objects.get(story_id = s_id)
+			except:
+				verified_response.append(s_id)
+	elif category_name =="jobstories":
+		for s_id in response_value:
+			try:
+				story_in_db_check = Job.objects.get(story_id = s_id)
+			except:
+				verified_response.append(s_id)
+	elif category_name == "newstories":
+		for s_id in response_value:
+			try:
+				story_in_db_check = New.objects.get(story_id = s_id)
+			except:
+				verified_response.append(s_id)
+
+	return verified_response
 
 def get_stories_list(category_list):
 	stories_list = []
@@ -35,10 +65,6 @@ def get_stories_list(category_list):
 def write_db(category_name,stories_list):
 	if category_name == "askstories":
 		for story in stories_list:
-			try:
-				story_in_db_check = Ask.objects.get(story_id = story["id"])
-			except:
-				print(story.get('id'))
 				obj = Ask.objects.get_or_create(
 					story_id = story.get('id'),
 					story_type = story.get('type'),
@@ -51,48 +77,36 @@ def write_db(category_name,stories_list):
 
 	elif category_name =="showstories":
 		for story in stories_list:
-			try:
-				story_in_db_check = Show.objects.get(story_id = story["id"])
-			except:
-				print(story.get('id'))
-				obj = Show.objects.get_or_create(
-					story_id = story.get('id'),
-					story_type = story.get('type'),
-					by = story.get('by'),
-					timestamp = story.get('time'),
-					title = story.get('title'),
-					text = story.get('text'),
-					url = story.get('url'),
-					)
+			obj = Show.objects.get_or_create(
+				story_id = story.get('id'),
+				story_type = story.get('type'),
+				by = story.get('by'),
+				timestamp = story.get('time'),
+				title = story.get('title'),
+				text = story.get('text'),
+				url = story.get('url'),
+				)
 
 	elif category_name =="jobstories":
 		for story in stories_list:
-			try:
-				story_in_db_check = Job.objects.get(story_id = story["id"])
-			except:
-				print(story.get('id'))
-				obj = Job.objects.get_or_create(
-					story_id = story.get('id'),
-					story_type = story.get('type'),
-					by = story.get('by'),
-					timestamp = story.get('time'),
-					title = story.get('title'),
-					text = story.get('text'),
-					url = story.get('url'),
-					)
+			obj = Job.objects.get_or_create(
+				story_id = story.get('id'),
+				story_type = story.get('type'),
+				by = story.get('by'),
+				timestamp = story.get('time'),
+				title = story.get('title'),
+				text = story.get('text'),
+				url = story.get('url'),
+				)
 
 	elif category_name == "newstories":
 		for story in stories_list:
-			try:
-				story_in_db_check = New.objects.get(story_id = story["id"])
-			except:
-				print(story.get('id'))
-				obj = New.objects.get_or_create(
-					story_id = story.get('id'),
-					story_type = story.get('type'),
-					by = story.get('by'),
-					timestamp = story.get('time'),
-					title = story.get('title',),
-					text = story.get('text'),
-					url = story.get('url'),
-					)
+			obj = New.objects.get_or_create(
+				story_id = story.get('id'),
+				story_type = story.get('type'),
+				by = story.get('by'),
+				timestamp = story.get('time'),
+				title = story.get('title',),
+				text = story.get('text'),
+				url = story.get('url'),
+				)
